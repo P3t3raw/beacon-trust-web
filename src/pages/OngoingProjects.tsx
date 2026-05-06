68ca12203a9fff0d1592c1396949d7151ff616cc
Fishbone Yahya <96491342+yahyafishbone@users.noreply.github.com>
2026-05-07 01:41:19 +0300

    Update OngoingProjects.tsx

--- src/pages/OngoingProjects.tsx
+++ src/pages/OngoingProjects.tsx
@@ -37,7 +37,6 @@ import kenyaVilla from "../assets/kenya-villa-1.jpg";
 import cbdkisumu1 from "../assets/cbdkisumu1.jpg";
 import chaberahbay from "../assets/chaberahbay.jpg";
 import nyawarakisian1 from "../assets/nyawarakisian1.jpg";
-import Ojolakisian1 from "../assets/Ojolakisian1.jpg"; 
 
 const OngoingProjects = () => {
   useSEO({
