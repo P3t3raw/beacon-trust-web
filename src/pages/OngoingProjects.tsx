c8cc041e912fd90aada495074a5a52c739a31f20
Fishbone Yahya <96491342+yahyafishbone@users.noreply.github.com>
2026-05-07 03:16:14 +0300

    Update OngoingProjects.tsx

--- src/pages/OngoingProjects.tsx
+++ src/pages/OngoingProjects.tsx
@@ -38,6 +38,7 @@ import cbdkisumu1 from "../assets/cbdkisumu1.jpg";
 import chaberahbay from "../assets/chaberahbay.jpg";
 import nyawarakisian1 from "../assets/nyawarakisian1.jpg";
 import Ojolakisian1 from "../assets/Ojolakisian1.jpg";
+
 const OngoingProjects = () => {
   useSEO({
     title: 'Ongoing Projects & Properties',
@@ -50,6 +51,7 @@ const OngoingProjects = () => {
     useScrollAnimation();
   const { elementRef: listingsRef, isVisible: listingsVisible } =
     useScrollAnimation();
+  
   const propertyListings = {
     houses: [
       {
